092d85dbebee7fb28ac9cf76a13f5884d46714c8
jaclynELF <jaclyn@englishlawfirm.co>
2025-04-02 18:48:40 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import EstatePlanningPitchDeck from "@/components/EstatePlanningPitchDeck";
+
+export default function Home() {
+  return (
+    <div>
+      <EstatePlanningPitchDeck />
+    </div>
+  );
+}
